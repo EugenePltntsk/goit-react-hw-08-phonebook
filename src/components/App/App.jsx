64ffe2dd75202c710d5980db0db1341381c5e63f
EugenePltntsk <eugeneplotnytskyi@gmail.com>
2022-11-17 21:04:11 +0200

    fixes

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,7 +1,7 @@
 import { Suspense, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { lazy } from 'react';
-import { Route, Routes } from 'react-router-dom';
+import { Navigate, Route, Routes } from 'react-router-dom';
 import { currentUserThunk } from 'redux/auth/authOperations';
 import { isRefreshingUserSelector } from 'redux/selectors';
 import PublicRoute from 'components/PublicRoute/PublicRoute';
@@ -29,6 +29,7 @@ export const App = () => {
           <Container>
             <Suspense fallback={<div>Loading...</div>}>
               <Routes>
+               
                 <Route path="/" element={<PublicRoute />}>
                   <Route path="/register" element={<RegisterPage />} />
                   <Route path="/login" element={<LoginPage />} />
@@ -36,6 +37,7 @@ export const App = () => {
                 <Route path="/" element={<PrivateRoute />}>
                   <Route path="/contacts" element={<ContactsPage />} />
                 </Route>
+                <Route path="*" element={<Navigate to="/login"/>}/>
               </Routes>
             </Suspense>
           </Container>
